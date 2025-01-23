1a3d0a26070ce3a874ddd12615ac43ef34455374
Jmchavarria <marlon05chavarri@gmail.com>
2025-01-23 15:24:51 -0500

    Remove dist folder from tracking and update .gitignore

--- dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-const db_config_1 = require("./db.config");
-Object.defineProperty(exports, "connectDB", { enumerable: true, get: function () { return db_config_1.connectDB; } });
